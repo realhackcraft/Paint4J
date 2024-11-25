61d69a369c7f62bc80ed7b12de3ef067ce2bb2a9
Noeidjdjeieidkd <noeidjdjeieidkd@gmail.com>
2024-11-25 14:40:05 -0500

    Update Pane.java

--- src/main/java/net/paintco/paint/Pane.java
+++ src/main/java/net/paintco/paint/Pane.java
@@ -1,9 +1,33 @@
 package net.paintco.paint;
 
-public class Pane {
+public class Pane implements Drawable{
   Drawable[][] content;
+  private int width;
+  private int height;
 
   public Pane(int width, int height) {
+    this.width = width;
+    this.height = height;
     content = new Drawable[width][height]();
+  }
+
+  @Override
+  public String getString() {
+    String[][] strings = new String[width][height]();
+    for (int i = 0; i < height; i++) {
+      Drawable[] col = content[i];
+      String row = "";
+      for (Drawable element : col) {
+        row += element.getString();
+      }
+      strings[i] = row;
+    }
+
+    return Utils.makeBox(content);
+  }
+
+  @Override
+  public void setString(String string) {
+    
   }
 }
