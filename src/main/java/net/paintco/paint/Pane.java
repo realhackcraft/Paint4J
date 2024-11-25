e49b1d8b00c2c1edd236f030199d5108acde501f
Hackcraft_ <hackcraft_@hotmail.com>
2024-11-25 15:10:54 -0500

    Move makeBox into pane

--- src/main/java/net/paintco/paint/Pane.java
+++ src/main/java/net/paintco/paint/Pane.java
@@ -8,9 +8,9 @@ public class Pane implements Drawable {
   public Pane(int width, int height) {
     this.width = width;
     this.height = height;
-    content = new Drawable[width][height];
-    for (int i = 0; i < width; i++) {
-      for (int j = 0; j < height; j++) {
+    content = new Drawable[height][width];
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
         content[i][j] = new NotDrawable();
       }
     }
@@ -18,10 +18,9 @@ public class Pane implements Drawable {
 
   @Override
   public String getString() {
-    String[] strings = new String[width];
-    for (int i = 0; i < width; i++) {
+    String[] strings = new String[height];
+    for (int i = 0; i < height; i++) {
       Drawable[] col = content[i];
-      System.out.println(i);
       String row = "";
       for (Drawable element : col) {
         row += element.getString();
@@ -29,14 +28,43 @@ public class Pane implements Drawable {
       strings[i] = row;
     }
 
-    return Utils.makeBox(strings);
+    return makeBox(strings);
   }
 
   public void setDrawable(int x, int y, Drawable drawable) {
-    content[y][x] = drawable;
+    content[x][y] = drawable;
   }
 
   @Override
   public void setString(String string) {
+  }
+
+  private String makeBox(String[] inputs) {
+    String out = "┌";
+    int max = 0;
+    for (int i = 0; i < inputs.length; i++) {
+      if (inputs[i].length() > max)
+        max = inputs[i].length();
+    }
+
+    for (int i = 0; i < max; i++) {
+      out += "─";
+    }
+    out += "┐\n";
+    for (String input : inputs) {
+      int filler = max - input.length();
+      out += "│" + input;
+      for (int i = 0; i < filler; i++) {
+        out += " ";
+      }
+      out += "│\n";
+    }
+
+    out += "└";
+    for (int i = 0; i < max; i++) {
+      out += "─";
+    }
+    out += "┘";
+    return out;
   }
 }
