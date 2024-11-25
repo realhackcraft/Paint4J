e373f60661589d325a0b0bc0efd69a8a50f62421
Hackcraft_ <hackcraft_@hotmail.com>
2024-11-25 13:42:01 -0500

    Add the package to Pixel.java

--- src/main/java/net/paintco/paint/Pixel.java
+++ src/main/java/net/paintco/paint/Pixel.java
@@ -1,59 +1,61 @@
+package net.paintco.paint;
+
 public class Pixel {
-    private boolean hasCursor;
-    private boolean isFilled;
+  private boolean hasCursor;
+  private boolean isFilled;
 
-    public Pixel(boolean hasCursor) {
-        this.hasCursor = hasCursor;
-        isFilled = false;
-    }
+  public Pixel(boolean hasCursor) {
+    this.hasCursor = hasCursor;
+    isFilled = false;
+  }
 
-    public Pixel() {
-        this(false);
-    }
+  public Pixel() {
+    this(false);
+  }
 
-    // No pixel character yet
-    public String returnPixel() {
-        if (hasCursor) {
-            if (isFilled) {
-                // filled pixel with cursor
-                return "▣";
-            } else {
-                // unfilled pixel with cursor
-                return "□";
-            }
-        } else {
-            if (isFilled) {
-                // filled pixel without cursor
-                return "█";
-            } else {
-                // unfilled pixel without cursor
-                return " ";
-            }
-        }
+  // No pixel character yet
+  public String returnPixel() {
+    if (hasCursor) {
+      if (isFilled) {
+        // filled pixel with cursor
+        return "▣";
+      } else {
+        // unfilled pixel with cursor
+        return "□";
+      }
+    } else {
+      if (isFilled) {
+        // filled pixel without cursor
+        return "█";
+      } else {
+        // unfilled pixel without cursor
+        return " ";
+      }
     }
+  }
 
-    // Getters and Setters;
-    public boolean hasCursor() {
-        return hasCursor;
-    }
+  // Getters and Setters;
+  public boolean hasCursor() {
+    return hasCursor;
+  }
 
-    public void removeCursor() {
-        hasCursor = false;
-    }
+  public void removeCursor() {
+    hasCursor = false;
+  }
 
-    public void giveCursor() {
-        hasCursor = true;
-    }
+  public void giveCursor() {
+    hasCursor = true;
+  }
 
-    public boolean isFilled() {
-        return isFilled;
-    }
+  public boolean isFilled() {
+    return isFilled;
+  }
 
-    public void fill() {
-        isFilled = true;
-    }
+  public void fill() {
+    isFilled = true;
+  }
 
-    public void empty() {
-        isFilled = false;
-    }
+  public void empty() {
+    isFilled = false;
+  }
 }
