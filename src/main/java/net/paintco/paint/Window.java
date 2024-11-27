e6b88cc3ace93b6b4cefa3d3a4eef9f0610c31ee
Hackcraft_ <hackcraft_@hotmail.com>
2024-11-27 14:43:56 -0500

    Fix window

--- src/main/java/net/paintco/paint/Window.java
+++ src/main/java/net/paintco/paint/Window.java
@@ -1,29 +1,31 @@
 package net.paintco.paint;
 
-public class Window extends Pane{
-    private int cursorX, int cursorY;
+public class Window extends Pane {
+  private int cursorX, cursorY;
 
-    public Window(){
-        cursorX=0;
-        cursorY=0;
-    }
+  public Window(int x, int y) {
+    super(x, y);
+    cursorX = 0;
+    cursorY = 0;
+  }
 
-    public moveX(int step){
-        cursorX+=step;
-        cursorX=Math.max(0, cursorX);
-        cursorX=Math.min(width, cursorX);
-    }
+  public void moveX(int step) {
+    cursorX += step;
+    cursorX = Math.max(0, cursorX);
+    cursorX = Math.min(width, cursorX);
+  }
 
-    public void moveY(int step){
-        cursorY+=step;
-        cursorY=Math.max(0, cursorY);
-        cursorY=Math.min(height, cursorY);
-    }
+  public void moveY(int step) {
+    cursorY += step;
+    cursorY = Math.max(0, cursorY);
+    cursorY = Math.min(height, cursorY);
+  }
 
-    @Override
-    public void setDrawable(int x, int y, Drawable drawable) {}
+  @Override
+  public void setDrawable(int x, int y, Drawable drawable) {
+  }
 
-    public void setPixel(Pixel p){
-        content[cursorY][cursorX]=p;
-    }
+  public void setPixel(Pixel p) {
+    content[cursorY][cursorX] = p;
+  }
 }
