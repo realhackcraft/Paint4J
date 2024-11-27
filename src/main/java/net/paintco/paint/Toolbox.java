635fe4166dad3de51a59ddfde52eb29360976fa5
Hackcraft_ <hackcraft_@hotmail.com>
2024-11-27 14:43:49 -0500

    Fix toolbox

--- src/main/java/net/paintco/paint/Toolbox.java
+++ src/main/java/net/paintco/paint/Toolbox.java
@@ -1,101 +1,104 @@
-package net.paint.paintco;
+package net.paintco.paint;
 
 public class Toolbox extends Pane {
-    private String[] labels;
-    private int selected = 0;
+  private String[] labels;
+  private int selected = 0;
 
-    public Button(String[] string) {
-        this.labels = string;
+  public Toolbox(String[] string) {
+    // We don't need width and height, the makeBox code is different
+    super(0, 0);
+    this.labels = string;
+  }
+
+  private String makeBox(String[] inputs, int selected) {
+    String out = "";
+    int[] lengths = new int[labels.length];
+    for (int i = 0; i < labels.length; i++) {
+      lengths[i] = labels[i].length();
     }
 
-    private String makeBox(String[] inputs, int selected) {
-        String out = "";
-        int[] lengths = new int[labels.length];
-        for (int i = 0; i < labels.length; i++) {
-            lengths[i] = labels[i];
+    for (int i = 0; i < labels.length; i++) {
+      int length = lengths[i];
+
+      if (selected == i) {
+        out += "┌";
+      } else {
+        out += " ";
+      }
+
+      for (int j = 0; j < length - 2; j++) {
+        if (selected == i) {
+          out += "─";
         }
+      }
 
-        for (int i = 0; i < labels.length; i++) {
-          int length = lengths[i];
+      if (selected == i) {
+        out += "┐";
+      } else {
+        out += " ";
+      }
+    }
+    out += "\n";
 
-          if (selected == i) {
-            out += "┌";
-          } else {
-            out += " ";
-          }
+    for (int i = 0; i < labels.length; i++) {
+      if (selected == i) {
+        out += "│";
+      } else {
+        out += " ";
+      }
+      out += labels[i];
+      if (selected == i) {
+        out += "│";
+      } else {
+        out += " ";
+      }
+    }
+    out += "\n";
 
-          for (int j = 0; j < length - 2; j++) {
-            if (selected == i) {
-            out += "─";
-            }
-          }
+    for (int i = 0; i < labels.length; i++) {
+      int length = lengths[i];
 
-          if (selected == i) {
-            out += "┐";
-          } else {
-            out += " ";
-          }
+      if (selected == i) {
+        out += "└";
+      } else {
+        out += " ";
+      }
+
+      for (int j = 0; j < length - 2; j++) {
+        if (selected == i) {
+          out += "─";
         }
-        out += "\n";
+      }
 
-        for (int i = 0; i < labels.length; i++) {
-          if (selected == i) {
-            out += "│";
-          } else {
-            out += " ";
-          }
-            out += labels[i];
-          if (selected == i) {
-            out += "│";
-          } else {
-            out += " ";
-          }
-        }
-        out += "\n";
-        
-        for (int i = 0; i < labels.length; i++) {
-          int length = lengths[i];
-
-          if (selected == i) {
-            out += "└";
-          } else {
-            out += " ";
-          }
-
-          for (int j = 0; j < length - 2; j++) {
-            if (selected == i) {
-            out += "─";
-            }
-          }
-
-          if (selected == i) {
-            out += "┘";
-          } else {
-            out += " ";
-          }
-        }
+      if (selected == i) {
+        out += "┘";
+      } else {
+        out += " ";
+      }
     }
+    return out;
+  }
 
-    public void setString(String string, int index) {
-        labels[index] = string;
-    }
+  public void setString(String string, int index) {
+    labels[index] = string;
+  }
 
-    public int getSelected() {
-        return selected;
-    }
+  public int getSelected() {
+    return selected;
+  }
 
-    public void setSelected(int index) {
-        if (index < labels.length) {
-            selected = index
-        }
+  public void setSelected(int index) {
+    if (index < labels.length) {
+      selected = index;
     }
+  }
 
-    @Override
-    public String getString() {
-        
-    }
+  @Override
+  public String getString() {
+    return makeBox(labels, selected);
+  }
 
-    @Override
-    public void setDrawable(int x, int y, Drawable drawable) {
-    }
+  @Override
+  public void setDrawable(int x, int y, Drawable drawable) {
+  }
 }
